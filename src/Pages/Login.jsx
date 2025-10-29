3c496c5d76591f77d780ab9f68937f3775ea48c6
unknown <lima-akter-p@example.com>
2025-10-30 01:19:03 +0600

    added resolve part

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -101,7 +101,7 @@ const Login = () => {
                                 <div>
                                     <button type="button" onClick={handleSigninWithGoole} 
 
-                                        className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-red-600 transition">
+                                        className=" w-full mt-3   bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-red-600 transition">
                                         Sign in with Google
                                     </button>
                                 </div>
